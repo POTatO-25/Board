e649281640e692495e4baf4678285bd9ed7e3e48
POTato <minji7658@gmail.com>
2025-03-09 18:10:04 +0900

    Question 클래스 @OneToMany 필드 추가

--- src/main/java/com/sbs/qna_service/boundedContext/question/Question.java
+++ src/main/java/com/sbs/qna_service/boundedContext/question/Question.java
@@ -1,10 +1,12 @@
 package com.sbs.qna_service.boundedContext.question;
 
+import com.sbs.qna_service.boundedContext.answer.Answer;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @AllArgsConstructor
 @Data
@@ -21,6 +23,12 @@ public class Question {
     private String content;
 
     private LocalDateTime createDate; // DATETIME
+
+    // @OneToMany : 자바의 편의를 위해 필드 생성된 것으로, 실제 DB 테이블에 컬럼 생성되지 X
+    // DB는 리스트나 배열을 만들 수 없기 때문
+    // CascadeType.REMOVE : 질문이 삭제되면 답변도 같이 삭제
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
 
     public Question() {
 
