1335320b91e33b0af12a5f3824469c0e9fd48a9a
POTato <minji7658@gmail.com>
2025-03-10 23:17:50 +0900

    JUnit test(SELECT Query 실행)

--- src/test/java/com/sbs/qna_service/BoardApplicationTests.java
+++ src/test/java/com/sbs/qna_service/BoardApplicationTests.java
@@ -8,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class BoardApplicationTests {
@@ -30,5 +34,63 @@ class BoardApplicationTests {
 		q2.setContent("id는 자동으로 생성되나요?");
 		q2.setCreateDate(LocalDateTime.now());
 		questionRepository.save(q2); // 두 번째 질문 저장
+	}
+
+	/*
+	* findAll() : SELECT * FROM question;
+	* */
+	@Test
+	@DisplayName("findAll")
+	void testCase002() {
+		List<Question> all = questionRepository.findAll();
+		assertEquals(2, all.size());
+
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+
+	/*
+	*  findById() : SELECT * FROM question WHERE id = 1;
+	* */
+	@Test
+	@DisplayName("findById")
+	void testCase003() {
+		Optional<Question> oq = questionRepository.findById(1);
+		if(oq.isPresent()) { // isPresent : 값의 존재를 확인
+			Question q = oq.get();
+			assertEquals("sbb가 무엇인가요?", q.getSubject());
+		}
+	}
+
+	/*
+	 *  findBySubject() : SELECT * FROM question WHERE subject = 'sbb가 무엇인가요?';
+	 * */
+	@Test
+	@DisplayName("findBySubject")
+	void testCase004() {
+		Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
+		assertEquals(1, q.getId());
+	}
+
+	/*
+	 *  findBySubjectAndContent() : SELECT * FROM question WHERE subject = 'sbb가 무엇인가요?'
+	 *  AND content = 'sbb에 대해서 알고 싶습니다.';
+	 * */
+	@Test
+	@DisplayName("findBySubjectAndContent")
+	void testCase005() {
+		Question q = questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+		assertEquals(1, q.getId());
+	}
+
+	/*
+	 *  findBySubjectLike() : SELECT * FROM question WHERE subject like 'sbb%';
+	 * */
+	@Test
+	@DisplayName("findBySubjectLike")
+	void testCase006() {
+		List<Question> qList = questionRepository.findBySubjectLike("sbb%");
+		Question q = qList.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
 }
